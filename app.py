dac657817a16e9166468dfdea8bda562c0dad68d
Kaan Sezen <119515258+KaanSezen1923@users.noreply.github.com>
2024-06-07 17:55:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import requests
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=7064d9275fc8f2197acc5c50fe5fb2e1&language=en-US".format( movie_id)
+    url = "https://api.themoviedb.org/3/movie/{}?api_key="YOUR API KEY"&language=en-US".format( movie_id)
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
